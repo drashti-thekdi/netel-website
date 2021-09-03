03601d1612ae24208f2178ca6c944f6faa904eff
drashti <riddhithekdi11111@gmail.com>
2021-09-03 17:18:17 +0530

    final

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// $(".toggle").click(function(){
+//     $('ul').slideToggle('Fast',function(){
+//         if($(this).is(':visible'))
+//         $(this).css('display','flex')
+//     })
+// })
+
+
+$(".toggle").click(function(){
+     $('ul').slideToggle();
+});
